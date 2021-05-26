c5a361d1daa6141791665bedd4af9444c0cb11f3
David Mosquera <al.mosqueradavid@gmail.com>
2021-05-25 20:55:15 -0500

    [Components]: Create Form component and add info handling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from "../src/components/Header";
+import Form from "../src/components/Form";
 import "./styles/index.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         Hola, bienvenido, sabemos que quieres viajar en un X, por favor
         diligencia el siguiente formulario
       </h1>
+      <Form />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,63 @@
-import React from "react";
+import { useState } from "react";
 
 const Form = () => {
+  const initialState = {
+    name: "",
+    email: "",
+    phone: "",
+  };
+
+  const [info, setInfo] = useState(initialState);
+
+  const handleInfoChange = (e) => {
+    console.log(info);
+    setInfo({
+      ...info,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("Your data -->", info);
+    setInfo(initialState);
+  };
+
   return (
     <>
-      <form>
-        <label for="name">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">
           <span>Ingresa tu nombre</span>
-          <input type="text" id="nombre" />
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={info.name}
+            onChange={handleInfoChange}
+          />
         </label>
-        <label for="email">
+        <label htmlFor="email">
           <span>Ingresa tu email</span>
-          <input type="email" id="email" />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={info.email}
+            onChange={handleInfoChange}
+          />
         </label>
-        <label for="celular">
+        <label htmlFor="phone">
           <span>Ingresa tu celular</span>
-          <input type="celular" id="celular" />
+          <input
+            type="tel"
+            id="phone"
+            name="phone"
+            value={info.phone}
+            onChange={handleInfoChange}
+          />
         </label>
+        <button type="submit">SEND</button>
       </form>
     </>
   );
